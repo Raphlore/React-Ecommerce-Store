7806f49c97b90e652bd19b2c1ef0b667435b098d
Raphlore <raphlore@yahoo.com>
2024-05-02 20:21:11 +0100

    My first commit

---
+++ src/products.js
@@ -0,0 +1,66 @@
+import product1 from './assets/iphone1.jpg'
+import product2 from './assets/camera.jpg'
+import product3 from './assets/laptop.jpg'
+import product4 from './assets/jacket.jpeg'
+import product5 from './assets/cap.jpg'
+import product6 from './assets/hoodie.jpeg'
+import product7 from './assets/airpods.png'
+import product8 from './assets/nike.jpg'
+import product9 from './assets/ring light.jpeg'
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: 'Iphone 15 Pro-Max',
+    price: 999.9,
+    productImage: product1
+  },
+  {
+    id: 2,
+    productName: '4K HD Camera',
+    price: 351.3,
+    productImage: product2
+  },
+  {
+    id: 3,
+    productName: 'Marcbook Pro 2022 (MI)',
+    price: 995.9,
+    productImage: product3
+  },
+  {
+    id: 4,
+    productName: 'Baseball Jacket',
+    price: 188.0,
+    productImage: product4
+  },
+  {
+    id: 5,
+    productName: 'Caps',
+    price: 20.7,
+    productImage: product5
+  },
+  {
+    id: 6,
+    productName: 'Hoodie Men',
+    price: 99.5,
+    productImage: product6
+  },
+  {
+    id: 7,
+    productName: 'Apple AirPods Pro (2nd Generation)',
+    price: 461.7,
+    productImage: product7
+  },
+  {
+    id: 8,
+    productName: ' Nike air force 1',
+    price: 790.5,
+    productImage: product8
+  },
+  {
+    id: 9,
+    productName: '14" 54cm Lead Soft Ring light & 1 Phone Holder',
+    price: 199.4,
+    productImage: product9
+  },
+]
